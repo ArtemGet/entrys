d8658693dd47764ba68fa2d88aab3afc8d5f522a
Artem Getmanskii <re910@yandex.ru>
2025-04-24 15:02:46 +0300

    add entry integer property

---
+++ src/main/java/io/github/artemget/EPropInt.java
@@ -0,0 +1,64 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2024-2025. Artem Getmanskii
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package io.github.artemget;
+
+/**
+ * Integer property entry.
+ *
+ * @since 0.0.1
+ */
+public final class EPropInt implements Entry<Integer> {
+    /**
+     * Origin entry.
+     */
+    private final Entry<String> origin;
+
+    /**
+     * Ctor.
+     *
+     * @param name Property
+     */
+    public EPropInt(final String name) {
+        this(new EProp(name));
+    }
+
+    /**
+     * Main Ctor.
+     *
+     * @param origin Property
+     */
+    public EPropInt(final Entry<String> origin) {
+        this.origin = origin;
+    }
+
+    @Override
+    public Integer value() throws EntryException {
+        try {
+            return Integer.valueOf(this.origin.value());
+        } catch (final NumberFormatException exception) {
+            throw new EntryException("Wrong entry type for int entry", exception);
+        }
+    }
+}
