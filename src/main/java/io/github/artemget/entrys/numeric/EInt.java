c94ab874a7be9afc89d36f7eae5b1596cd10e30e
Artem Getmanskii <re910@yandex.ru>
2025-05-12 20:49:23 +0300

    moved to entrys prefix

---
+++ src/main/java/io/github/artemget/entrys/numeric/EInt.java
@@ -0,0 +1,57 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2024-2025. Artem Getmanskii
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package io.github.artemget.entrys.numeric;
+
+import io.github.artemget.entrys.ESafe;
+import io.github.artemget.entrys.Entry;
+import io.github.artemget.entrys.EntryException;
+
+/**
+ * Integer entry.
+ * @since 0.2.0
+ */
+public final class EInt implements Entry<Integer> {
+    /**
+     * Origin entry.
+     */
+    private final Entry<String> origin;
+
+    /**
+     * Main Ctor.
+     * @param origin Property
+     */
+    public EInt(final Entry<String> origin) {
+        this.origin = new ESafe<>(origin);
+    }
+
+    @Override
+    public Integer value() throws EntryException {
+        try {
+            return Integer.valueOf(this.origin.value());
+        } catch (final NumberFormatException exception) {
+            throw new EntryException("Wrong entry type for int entry", exception);
+        }
+    }
+}
