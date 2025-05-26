5a84c67ac3ec3e233ff001666fd0ea736b26f114
Artem Getmanskii <re910@yandex.ru>
2025-05-26 13:58:29 +0300

    add temp EVal

---
+++ src/main/java/io/github/artemget/entrys/file/EVal.java
@@ -0,0 +1,53 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2024-2025. Artem Getmanskii
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package io.github.artemget.entrys.file;
+
+import com.amihaiemil.eoyaml.Yaml;
+import com.amihaiemil.eoyaml.YamlMapping;
+import io.github.artemget.entrys.ESafe;
+import java.io.IOException;
+
+public final class EVal extends ESafe<String> {
+
+    public EVal(final String key) {
+        this(key, "/src/main/resources/application.yaml");
+    }
+
+    public EVal(final String key, final String path) {
+        super(
+            () -> {
+                YamlMapping root;
+                try {
+                    root = Yaml.createYamlInput(new EFile(path).value())
+                        .readYamlMapping();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+
+            },
+            () -> String.format("Attribute for key '%s' is null for path '%s'", key, path)
+        );
+    }
+}
