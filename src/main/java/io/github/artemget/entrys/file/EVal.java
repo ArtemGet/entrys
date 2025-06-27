89952a075ffd5ba14af9e141bf22f8e49e308083
Artem Getmanskii <re910@yandex.ru>
2025-06-28 00:17:07 +0300

    add sequence flow

--- src/main/java/io/github/artemget/entrys/file/EVal.java
+++ src/main/java/io/github/artemget/entrys/file/EVal.java
@@ -26,7 +26,9 @@ package io.github.artemget.entrys.file;
 
 import com.amihaiemil.eoyaml.Yaml;
 import com.amihaiemil.eoyaml.YamlMapping;
+import com.amihaiemil.eoyaml.YamlNode;
 import io.github.artemget.entrys.ESafe;
+import io.github.artemget.entrys.Entry;
 import io.github.artemget.entrys.EntryException;
 import io.github.artemget.entrys.operation.ESplit;
 import java.io.IOException;
@@ -39,33 +41,34 @@ public final class EVal extends ESafe<String> {
     }
 
     public EVal(final String key, final String path) {
+        this(key, new EFile(path));
+    }
+
+    public EVal(final String key, final Entry<String> content) {
         super(
             () -> {
                 YamlMapping root;
                 try {
-                    root = Yaml.createYamlInput(new EFile(path).value())
+                    root = Yaml.createYamlInput(content.value())
                         .readYamlMapping();
-                } catch (IOException exception) {
-                    throw new EntryException(
-                        String.format(
-                            "Failed to read yaml mapping for key: '%s', for path: '%s'",
-                            key,
-                            path
-                        ),
-                        exception
-                    );
+                } catch (IOException | EntryException exception) {
+                    throw new EntryException(String.format("Failed to read yaml mapping for key: '%s'", key), exception);
                 }
                 String res = null;
                 List<String> elements = new ESplit(() -> key, ".").value();
                 for (int i = 0; i < elements.size(); i++) {
                     if (i == elements.size() - 1) {
-                        res = root.string(elements.get(i));
+                        final YamlNode value = root.value(elements.get(i));
+                        switch (value.type()) {
+                            case SCALAR ->  res = value.toString();
+                            case SEQUENCE -> res = value.asSequence().toString();
+                        }
                     }
                     root = root.yamlMapping(elements.get(i));
                 }
                 return res;
             },
-            () -> String.format("Attribute for key '%s' is null for path '%s'", key, path)
+            () -> String.format("Attribute for key '%s' is null", key)
         );
     }
 }
